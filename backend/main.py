68d58c7c670edfbe8ccc3f586ea3da796e36272c
qozphobia <chelowekprosto79@gmail.com>
2026-01-21 20:57:30 +0100

    B-01 finished

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,22 @@
-def main():
-    print("Hello, World!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from database import engine
+from models import Base
 
-if __name__ == "__main__":
-    main()
+Base.metadata.create_all(bind=engine)
+app = FastAPI()
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # для розробки (потім краще вказати конкретні домени)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def root():
+    return {"message": "FastAPI is working"}
